e1c42c738094af6c19c20cd9c6d719078edbfd87
Christina von Flach <christina.flach@gmail.com>
2019-11-01 17:28:56 -0300

    Estrutura para testar IntegerList

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,18 @@
-#include <division.h>
+#include <iostream>
+using namespace std;
+#include <IntegerList.h>
 
-static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
-static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
+static const char *const USAGE = "Usage:\n\t ... \n\nDescription:\n\tComputes ...\n";
 
 int main(int argc, const char *argv[]) {
-  Fraction f;
-
-  std::cout << HEADER;
 
   // ensure the correct number of parameters are used.
-  if (argc < 3) {
-    std::cout << USAGE;
-    return 1;
-  }
+  // if (argc < 3) {
+  //   cout << USAGE;
+  //   return 1;
+  // }
 
-  f.numerator = atoll(argv[1]);
-  f.denominator = atoll(argv[2]);
-
-  Division d = Division(f);
-  try {
-    DivisionResult r = d.divide();
-
-    std::cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
-    std::cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
-  } catch (DivisionByZero) {
-    std::cout << "Can not divide by zero, Homer. Sober up!\n";
-  }
+  // CODE
+ 
   return 0;
 }
--- test/src/integerlist_tests.cpp
+++ test/src/integerlist_tests.cpp
@@ -1,28 +1,21 @@
-//
-// Created by Konstantin Gredeskoul on 5/16/17.
-//
-#include <division.h>
+#include <IntegerList.h>
 #include "gtest/gtest.h"
 
-using namespace std;
-
-
-#define VI vector<long long>
-
-class DividerTest : public ::testing::Test {
+class IntegerListTest : public ::testing::Test {
 
 protected:
-  VI numerators   = {5, 9, 17, 933345453464353416L};
-  VI denominators = {2, 3, 19, 978737423423423499L};
-  VI divisions    = {2, 3, 0, 0};
-  VI remainders   = {1, 0, 17, 933345453464353416};
+  
+  IntegerList *listavazia = NULL;
+  IntegerList *a_node = NULL;
 
   virtual void SetUp() {
+     a_node = createElement(10);
   };
 
   virtual void TearDown() {
   };
 
+/*
   virtual void verify(int index) {
     Fraction       f        = Fraction{numerators.at(index), denominators.at(index)};
     DivisionResult expected = DivisionResult{divisions.at(index), remainders.at(index)};
@@ -30,16 +23,28 @@ protected:
     EXPECT_EQ(result.division, expected.division);
     EXPECT_EQ(result.remainder, expected.remainder);
   }
+*/
+
 };
 
-TEST_F(DividerTest, 5_DivideBy_2) {
-  verify(0);
+
+TEST_F(IntegerListTest, ListIsEmpty) {
+  EXPECT_TRUE(isEmpty(listavazia));
 }
 
-TEST_F(DividerTest, 9_DivideBy_3) {
-  verify(1);
+TEST_F(IntegerListTest, InsertFirstEmptySize) {
+  IntegerList *lista = NULL;
+  insertElement(&lista, 99);
+  EXPECT_EQ(size(lista), 1);
 }
 
+TEST_F(IntegerListTest, InsertFirstEmptySearch) {
+  IntegerList *lista = NULL;
+  insertElement(&lista, 99);
+  EXPECT_EQ(search(lista, 99), 1);
+}
+
+/*
 TEST_F(DividerTest, 17_DivideBy_19) {
   verify(2);
 }
@@ -60,4 +65,4 @@ TEST_F(DividerTest, DivisionByZero) {
     FAIL() << "Expected DivisionByZeroException!";
   }
 }
-
+*/
